2b8aeb72d265732e0444f15bb7ab15b3573aab8d
konrad920 <konradsowizral920@gmail.com>
2023-05-04 21:43:38 +0200

    entity.framework

---
+++ MotoApp/Data/MotoAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotoApp.Entities;
+
+namespace MotoApp.Data
+{
+    public class MotoAppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees => Set<Employee>();
+
+        public DbSet<BuisnessPartner> BuisnessPartners => Set<BuisnessPartner>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+        }
+    }
+}
