768b38bfa86e62e9fda632732ce68586c3f71746
konrad920 <konradsowizral920@gmail.com>
2023-05-05 19:15:44 +0200

    kowariancja i kontrawariancja oraz generyczne interface

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,10 +1,23 @@
-﻿using MotoApp.Entities;
+﻿using MotoApp.Data;
+using MotoApp.Entities;
 using MotoApp.Repositories;
 
-var repository = new GenericRepository<Employee>();
-repository.Add(new Employee { FirstName = "Adam" });
-repository.Add(new Employee { FirstName = "Konrad" });
-repository.Add(new Employee { FirstName = "Kamil" });
+//var repository = new GenericRepository<Employee>();
+//repository.Add(new Employee { FirstName = "Adam" });
+//repository.Add(new Employee { FirstName = "Konrad" });
+//repository.Add(new Employee { FirstName = "Kamil" });
+//repository.Save();
 
-repository.Save();
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
+employeeRepository.Add(new Employee { FirstName = "Konrad"});
+employeeRepository.Add(new Employee { FirstName = "Ela" });
+employeeRepository.Add(new Employee { FirstName = "Kamil" });
+employeeRepository.Save();
 
+GetEmployeeById(employeeRepository);
+
+static void GetEmployeeById(IReadRepository<IEntity> employeeRepository)
+{
+    var employee = employeeRepository.GetById(2);
+    Console.WriteLine(employee.ToString());
+}
--- MotoApp/Repositories/ListRepository.cs
+++ MotoApp/Repositories/ListRepository.cs
@@ -2,10 +2,15 @@
 
 namespace MotoApp.Repositories
 {
-    internal class GenericRepository<T>
-        where T : class, IEntity, new()
+    public class ListRepository<T> : IRepository<T> where T : class, IEntity, new()
     {
         protected readonly List<T> _items = new();
+
+        public IEnumerable<T> GetAll()
+        {
+            return _items.ToList();
+        }
+
         public T GetById(int id)
         {
             return _items.Single(item => item.Id == id);
